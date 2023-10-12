06cb2743c6b49a8de5ec25ce87db74565a51e60a
MasterpieceKODZ <grail.masterpiece@gmail.com>
2023-10-13 00:04:45 +0100

    wrote cypress e2e test for the get-started btn

---
+++ cypress/e2e/landing.page.ui.cy.ts
@@ -0,0 +1,5 @@
+it("verify UI into text and get started button should navigate user to login page", () => {
+	cy.visit("/").contains("GET STARTED").click();
+
+	cy.url().should("eq", `${Cypress.config().baseUrl}/login`);
+});
