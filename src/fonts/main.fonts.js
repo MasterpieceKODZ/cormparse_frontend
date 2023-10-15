69cb3d348de9e8b4ca615cb0fce80fbd04ac1e57
MasterpieceKODZ <grail.masterpiece@gmail.com>
2023-10-15 12:26:36 +0100

    retified issue with local font, problem was a wrong font obj style prop, solution: removed the weight and style prop from all font objs

--- src/fonts/main.fonts.js
+++ src/fonts/main.fonts.js
@@ -1,43 +1,38 @@
 import localFont from "next/font/local";
 
 export const londrina_outline = localFont({
-	src: "./Londrina_Outline//LondrinaOutline-Regular.ttf",
+	src: "Londrina_Outline/LondrinaOutline-Regular.ttf",
 	display: "swap",
 	subsets: ["latin"],
-	weight: "400",
 	variable: "--font-londrina-outline",
 });
 
 export const exo = localFont({
 	src: [
 		{
-			path: "./Exo/Exo-VariableFont_wght.ttf",
+			path: "Exo/Exo-VariableFont_wght.ttf",
+			//weight: ["variable"],
 			style: "normal",
 		},
 		{
-			path: "./Exo/Exo-Italic-VariableFont_wght.ttf",
+			path: "Exo/Exo-Italic-VariableFont_wght.ttf",
 			style: "italic",
 		},
 	],
-	weight: "variable",
-	style: ["italic", "normal"],
 	display: "swap",
 	subsets: ["latin"],
 	variable: "--font-exo",
 });
 
 export const russo_one = localFont({
-	src: "./Russo_One//RussoOne-Regular.ttf",
-	weight: "400",
+	src: "Russo_One/RussoOne-Regular.ttf",
 	display: "swap",
 	subsets: ["latin"],
 	variable: "--font-russo-one",
 });
 
 export const edu_tas_beg = localFont({
-	src: "./Edu_Tas_Beginner/EduTASBeginner-VariableFont_wght.ttf",
-	weight: "variable",
-	style: ["normal"],
+	src: "Edu_Tas_Beginner/EduTASBeginner-VariableFont_wght.ttf",
 	display: "swap",
 	subsets: ["latin"],
 	variable: "--font-edu-tas-beginner",
