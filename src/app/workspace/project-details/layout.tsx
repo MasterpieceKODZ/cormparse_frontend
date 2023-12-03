26d96218e1e0776e1107bd335d93e27e0a142a04
Gideon Faith Omorodion <grail.masterpiece@gmail.com>
2023-12-03 17:46:51 +0100

    began the projects details page UI, built the  side nav-pane (#18)

--- src/app/workspace/project-details/layout.tsx
+++ src/app/workspace/project-details/layout.tsx
@@ -1,6 +1,12 @@
+import ProjectDetailsLeftNavPane from "@/components/workspace/project-details/left.nav.pane";
 import "../../../styles/project.details.css";
 const ProjectDetailsLayout = ({ children }: { children: React.ReactNode }) => {
-	return <section className=" tw-w-full tw-min-h-full">{children}</section>;
+	return (
+		<section className=" tw-w-full tw-h-full">
+			<ProjectDetailsLeftNavPane name="Project Ambassadors Extraction" />
+			{children}
+		</section>
+	);
 };
 
 export default ProjectDetailsLayout;
--- src/components/workspace/project-details/left.pane.nav.btn.tsx
+++ src/components/workspace/project-details/left.pane.nav.btn.tsx
@@ -1,9 +1,14 @@
+"use client";
+import { projectsDetailsSidepaneButtonClick } from "@/ui_controllers/workspace/projects/details/side.pane.btn.click";
+
 const ProjectDetailLeftPaneNavBtn = ({ label }: { label: string }) => {
 	return (
 		<div
 			id="left_pane_btn_wrapper"
-			className=" tw-w-full tw-h-max tw-mt-4">
-			<button className=" tw-w-full tw-h-max tw-text-start tw-text-gray-800 dark:tw-text-gray-400">
+			className=" tw-w-full tw-h-max tw-mt-6 tw-py-2 side_pane_btn_wrapper">
+			<button
+				className=" tw-w-full tw-h-max tw-ps-3 tw-text-[17px] tw-text-start tw-text-gray-800 dark:tw-text-gray-400"
+				onClick={projectsDetailsSidepaneButtonClick}>
 				{label}
 			</button>
 		</div>
